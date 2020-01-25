e1bcbf3c3483c6dee9df5e5e1e5b7fca3ce7d276
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-01-24 23:46:06 -0300

    Added additional routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
@@ -24,6 +24,7 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema)
 
+////////////// Requests targetting all articles  //////////////////
 
 app.route('/articles')
   .get((req, res) => {
@@ -60,9 +61,68 @@ app.route('/articles')
   })
 })
 
-app.get('/:id', (req, res) => {
-  console.log(req.params.id)
-})
+////////////// Requests targetting all articles  //////////////////
+
+app.route('/articles/:articleTitle')
+  .get((req, res) => {
+    const { articleTitle } = req.params
+
+    Article.findOne({
+      title: articleTitle
+    }, (err, foundArticle) => {
+      if (foundArticle) {
+        res.status(200).json(foundArticle)
+      } else {
+        res.status(400).json({ message: 'No articles found'})
+      }
+    })
+  })
+
+  .put((req, res) => {
+    const { articleTitle } = req.params
+    const { title, content } = req.body
+    // console.log('passou')
+    // console.log(articleTitle,title,content)
+
+    Article.update(
+      {title: articleTitle},
+      {title, content},
+      {overwrite: true}),
+      (err) => {
+        if(!err) {
+          res.status(200).json({message: 'Article updated'})
+        }
+      }
+  })
+
+  .patch((req, res) => {
+    const { articleTitle } = req.params
+    console.log(req.body)
+    Article.update(
+      {title: articleTitle},
+      {$set: req.body},
+      (err) => {
+        if (!err) {
+          res.send('Updated article')
+        } else {
+          res.send(err)
+        }
+      }
+    )
+  })
+
+  .delete((req, res) => {
+    const { articleTitle } = req.params
+    console.log(articleTitle)
+
+    Article.findOneAndDelete({
+      title: articleTitle
+    }, (err) => {
+      if (err) {
+        throw(err)
+      } res.status(200).json('deleted')
+    })
+  })
 
 
 app.listen(3000, () => console.log('Server running on port #3000'))
